35eef3aa4a5059a120d09a0e0269c23de9c398c5
Sigfrid Stjärnholm <sigfrid@stjarnholm.com>
2025-03-23 01:40:09 +0100

    Add fix for None y values

--- core/pages/reactor_operating_data.py
+++ core/pages/reactor_operating_data.py
@@ -29,32 +29,24 @@ def reactor_operating_data():
         return start, stop
 
     @ui.refreshable
-    def plot_cards(
-        start_local: datetime | None = None, stop_local: datetime | None = None
-    ):
+    def plot_cards(start_local: datetime | None = None, stop_local: datetime | None = None):
         if start_local is None:
             start_local = datetime.now()
         if stop_local is None:
             stop_local = datetime.now()
 
-        start_earliest_on_local_day = start_local.replace(
-            hour=0, minute=0, second=0, microsecond=0
-        )
-        stop_latest_on_local_day = stop_local.replace(
-            hour=23, minute=59, second=59, microsecond=999999
-        )
+        start_earliest_on_local_day = start_local.replace(hour=0, minute=0, second=0, microsecond=0)
+        stop_latest_on_local_day = stop_local.replace(hour=23, minute=59, second=59, microsecond=999999)
 
         with ui.row().classes("items-center"):
-            ui.icon("edit_calendar", size="md", color="primary").on(
-                "click", date_range_menu.open
-            ).classes("cursor-pointer ml-2 hover:bg-slate-200 rounded-full h-12 w-12")
+            ui.icon("edit_calendar", size="md", color="primary").on("click", date_range_menu.open).classes(
+                "cursor-pointer ml-2 hover:bg-slate-200 rounded-full h-12 w-12"
+            )
             with ui.row().classes("text-lg font-mono"):
                 if start_local.date() == stop_local.date():
                     ui.markdown(f"Showing data from **{start_local.date()}**")
                 else:
-                    ui.markdown(
-                        f"Showing data from **{start_local.date()}** to **{stop_local.date()}**"
-                    )
+                    ui.markdown(f"Showing data from **{start_local.date()}** to **{stop_local.date()}**")
 
         ui.separator().classes("mb-2")
 
@@ -70,12 +62,8 @@ def reactor_operating_data():
                     with ui.card():
                         with ui.row().classes("w-full"):
                             with ui.row().classes("items-baseline"):
-                                ui.label(reactor.reactor_name).classes(
-                                    "text-lg font-bold font-mono"
-                                )
-                                ui.label(reactor.reactor_type).classes(
-                                    "text-xs font-mono"
-                                )
+                                ui.label(reactor.reactor_name).classes("text-lg font-bold font-mono")
+                                ui.label(reactor.reactor_type).classes("text-xs font-mono")
                         ui.label("No data")
                     continue
 
@@ -88,15 +76,15 @@ def reactor_operating_data():
                 # Loop over all x values. If there is more than time_window minutes between two x values, insert that time in x and a None value in y. This breaks the plot line if data is missing. Updates are expected every 10 minutes.
                 i = 0
                 while i < len(x) - 1:
-                    if (
-                        y[i] != None
-                        and x[i] + timedelta(minutes=time_window) < x[i + 1]
-                    ):
+                    if y[i] != None and x[i] + timedelta(minutes=time_window) < x[i + 1]:
                         x.insert(i + 1, x[i] + timedelta(minutes=time_window))
                         y.insert(i + 1, None)
                     i += 1
 
-                max_y_axis = max(100, max(y)) + 10
+                # As we might have None values
+                max_of_non_none_y = max([y for y in y if y is not None], default=0)
+
+                max_y_axis = max(100, max_of_non_none_y) + 10
                 fig = go.Figure(
                     go.Scatter(x=x, y=y),
                     layout=go.Layout(yaxis=dict(range=[0, max_y_axis])),
@@ -105,35 +93,25 @@ def reactor_operating_data():
                 with ui.card():
                     with ui.row().classes("w-full"):
                         with ui.row().classes("items-baseline"):
-                            ui.label(reactor.reactor_name).classes(
-                                "text-lg font-bold font-mono"
-                            )
+                            ui.label(reactor.reactor_name).classes("text-lg font-bold font-mono")
                             ui.label(reactor.reactor_type).classes("text-xs font-mono")
                         ui.space()
-                        ui.circular_progress(
-                            round(points[-1].fields["pct"]), min=0, max=100, size="md"
-                        ).classes("mr-2")
+                        ui.circular_progress(round(points[-1].fields["pct"]), min=0, max=100, size="md").classes("mr-2")
                     ui.plotly(fig).classes("w-96 h-40")
 
     with theme.frame():
         # Dates picker
         with ui.row():
             start_interval, stop_interval = get_start_stop_intervals()
-            start_interval_date_str = datetime_converter.utc_to_local(
-                start_interval
-            ).strftime("%Y/%m/%d")
-            stop_interval_date_str = datetime_converter.utc_to_local(
-                stop_interval
-            ).strftime("%Y/%m/%d")
+            start_interval_date_str = datetime_converter.utc_to_local(start_interval).strftime("%Y/%m/%d")
+            stop_interval_date_str = datetime_converter.utc_to_local(stop_interval).strftime("%Y/%m/%d")
 
             today = datetime.now(timezone.utc)
             if today < start_interval:
                 today = start_interval
             elif today > stop_interval:
                 today = stop_interval
-            today_interval_str_dashes = datetime_converter.utc_to_local(today).strftime(
-                "%Y-%m-%d"
-            )
+            today_interval_str_dashes = datetime_converter.utc_to_local(today).strftime("%Y-%m-%d")
 
             with ui.menu() as date_range_menu:
                 with ui.date(
